3c27581b06171d51333187b61a2902705e2ae261
Mustapha <jerbimustapha2@gmail.com>
2024-09-27 16:56:30 +0100

    Animal Addition, Search Functionality and delete function

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,24 +2,96 @@ public class Zoo {
     Animal[] animals;
     String name;
     String city;
-    int nbrCages;
+    final int nbrCages = 25;
+    int animalCount = 0;
 
     // Parameterized constructor
-    public Zoo(String name, String city, int nbrCages) {
+    public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
         animals = new Animal[nbrCages];
     }
 
+
+    // Méthode pour afficher le zoo
     public void displayZoo() {
         System.out.println("Zoo Name: " + name);
         System.out.println("City: " + city);
         System.out.println("Number of Cages: " + nbrCages);
+        System.out.println("Animals in the Zoo:");
+
+
+        for (int i = 0; i < animalCount; i++) {
+            System.out.println(animals[i].toString());
+        }
+    }
+
+    // Méthode pour chercher un animal selon son nom
+    public int searchAnimal(Animal animal) {
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i].name.equals(animal.name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    // Méthode pour ajouter un animal au zoo
+    public boolean addAnimal(Animal animal) {
+        // Vérifier si l'animal existe déjà
+        if (searchAnimal(animal) != -1) {
+            System.out.println("L'animal:"+animal.name +" déjà existe dans le zoo.");
+            return false;
         }
 
-    public String toString() {
-        return "Zoo Name: " + name + ", City: " + city + ", Number of Cages: " + nbrCages;
+        // Vérifier si le zoo est plein
+        if (animalCount < nbrCages) {
+            animals[animalCount] = animal;
+            animalCount++;
+            return true;
+        } else {
+            System.out.println("Le zoo est plein, impossible d'ajouter un autre animal.");
+            return false;
+        }
+
+
     }
+
+
+    // Méthode pour supprimer un animal du zoo
+    public boolean removeAnimal(Animal animal) {
+        int index = searchAnimal(animal); // Cherche l'animal
+
+        if (index != -1) { // Si l'animal est trouvé
+            // Décale tous les animaux suivants d'une position vers la gauche
+            for (int i = index; i < animalCount - 1; i++) {
+                animals[i] = animals[i + 1];
+            }
+            animals[animalCount - 1] = null; // Réinitialise la dernière position
+            animalCount--; // Diminue le compteur d'animaux
+            return true; // Suppression réussie
+        } else {
+            System.out.println("Cet animal n'existe pas dans le zoo.");
+            return false; // L'animal n'a pas été trouvé
+        }
+
+    }
+
+    // Méthode pour vérifier si le zoo est plein
+    public boolean isZooFull() {
+        return animalCount >= nbrCages; // Returns true if full
+    }
+
+    // Méthode pour comparer deux zoos et retourner celui avec le plus d'animaux
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.animalCount > z2.animalCount) {
+            return z1; // Retourner z1 si z1 a plus d'animaux
+        } else {
+            return z2; // Retourner z2 si z2 a plus d'animaux ou s'ils sont égaux
+        }
+    }
+
+
 
 }
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -8,14 +8,45 @@ public class ZooManagement {
         Animal lion = new Animal("Felidae", "Lion", 5, true);
 
         // Creating a zoo
-        Zoo myZoo = new Zoo("just_a_zoo", "tunisia", 10);
+        Zoo myZoo = new Zoo("just_a_zoo", "tunisia");
+
+        Animal tiger = new Animal("Felidae", "Tiger", 4, true);
+        Animal elephant = new Animal("Elephantidae", "Elephant", 10, true);
+        Animal zebra = new Animal("Equidae", "Zebra", 3, false);
+        Animal zebra1 = new Animal("Equidae", "Zebra", 3, false);
+
+
+        myZoo.addAnimal(zebra);
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(elephant);
+        myZoo.addAnimal(tiger);
+
+
+        if (myZoo.searchAnimal(zebra1) != 1) {
+            System.out.println("Animal trouvé à l'indice: " + myZoo.searchAnimal(zebra1));
+        } else {
+            System.out.println("Animal non trouvé.");
+        }
+
 
         myZoo.displayZoo();
 
+        myZoo.removeAnimal(lion);
+
+        myZoo.displayZoo();
+
+
         System.out.println(myZoo);
 
-        System.out.println(myZoo.toString());
+        System.out.println(myZoo);
         System.out.println(lion.toString());
+
+        Zoo myZoo1 = new Zoo("zoo1", "tunisia");
+        Zoo myZoo2 = new Zoo("zoo2", "sfax");
+        myZoo1.addAnimal(zebra);
+
+        Zoo largestZoo = Zoo.comparerZoo(myZoo1, myZoo2);
+        System.out.println("Le zoo avec le plus d'animaux est : " + largestZoo.name);
 
 
     }
